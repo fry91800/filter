49d38d81ac6301d1e1c2a17fb9559bfc3b2a167a
Francois Nguinbu <francois.nguinbu@epita.fr>
2024-12-01 22:27:26 +0100

    UI working but code is a mess, committing for the record, time to refactor

--- filter.js
+++ filter.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     Piece of code to add and delete filters every filter has a unique identifier to
     help destoy it later using the delete filter button
     */
+    var captureCurrentId = null
+    var captureDeleteId = null
     let filterCount = 0
     let evalElementCount = 0
     $("#filter-maker").on("click", function () {
@@ -45,8 +47,14 @@ $(document).ready(function () {
                 $(droppedElement).toggleClass("item")
                 $(droppedElement).toggleClass("eval-element")
                 $(droppedElement).attr('id', `eval-element${evalElementCount}`);
+                $(droppedElement).attr('data-unique', `${evalElementCount}`);
                 if ($(droppedElement).data("type") === "number") {
-                    $(droppedElement).text("0")
+                    $(droppedElement).html(`
+                            <span class="text">0</span>
+                             <div id="delete-eval-element${evalElementCount}" class="delete-eval-element" data-target="eval-element${evalElementCount}">
+                                x
+                             </div>
+                            `)
                 }
                 $(droppedElement).append(`
                 <div id="delete-eval-element${evalElementCount}" class="delete-eval-element" data-target="eval-element${evalElementCount}">
@@ -56,10 +64,71 @@ $(document).ready(function () {
                 $(this).append(droppedElement.outerHTML);
 
                 $(`#delete-eval-element${evalElementCount}`).on("click", function () {
-                    console.log("test")
                     const targetId = $(this).data("target");
                     $(`#${targetId}`).remove();
                 });
+
+                /* */
+                // When the div is clicked, turn it into an input field
+                $(`#eval-element${evalElementCount}`).find('.text').on('click', function () {
+                    var currentText = $(this).parent().data("value"); // Get the current text
+                    var currentId = $(this).parent().data("unique");
+                    console.log("clicked, id found is: " + currentId)
+                    $(this).parent().html(`<input type="text" id="input-${currentId}" data-unique="${currentId}" class="edit-input" value="` + currentText + '" />');
+
+                    // Focus on the input field
+                    $('.edit-input').focus();
+                });
+
+                // Validate the text when Enter is pressed
+                $(document).on('keypress', '.edit-input', function (e) {
+                    if (e.which === 13) { // Enter key
+                        var newText = $(this).val(); // Get the new text
+                        var id = $(this).data("unique")
+                        console.log("entered, id found is: " + id)
+                        $(this).parent().html(`
+                            <span class="text">${newText}</span>
+                             <div id="delete-eval-element${id}" class="delete-eval-element" data-target="eval-element${id}">
+                                x
+                             </div>
+                            `);
+                        $(`#eval-element${id}`).data("value", newText)
+                                // Restore data attributes
+                                $(this).parent().data("value", newText);
+                                $(this).parent().data("unique", id);
+                        // $(`#eval-element${evalElementCount}`).append(`
+                        //     <div id="delete-eval-element${evalElementCount}" class="delete-eval-element" data-target="eval-element${evalElementCount}">
+                        //        x
+                        //     </div>`)
+                        // Reset the trigger for input
+                        console.log("adding event listener for: " +id)
+                        $(`#eval-element${id}`).find('.text').on('click', function () {
+
+                            console.log(JSON.stringify($(this)))
+                            var currentText = $(this).parent().data("value"); // Get the current text
+                            var currentId = $(this).parent().data("unique");
+                            console.log("clicked, id passed is: " + currentId)
+                            $(this).parent().html(`<input type="text" data-unique="${currentId}" class="edit-input" value="` + currentText + '" />');
+
+                            // Focus on the input field
+                            $('.edit-input').focus();
+                        });
+                        // Reset the delete button trigger
+                        $(`#delete-eval-element${id}`).on("click", function () {
+                            const targetId = $(this).data("target");
+                            $(`#${targetId}`).remove();
+                        });
+                    }
+                });
+
+                // Optional: Close input if clicked outside
+                // $(document).on('click', function (e) {
+                //     if (!$(e.target).closest(`#eval-element${evalElementCount}`).length) {
+                //         var newText = $('.edit-input').val();
+                //         $(droppedElement).text(newText); // Set the new text
+                //     }
+                // });
+                /* */
                 // // Same, can't change that before dropping the element, doing it after
                 // $('.eval-element').on("click", function () {
                 //     $(this).remove();
